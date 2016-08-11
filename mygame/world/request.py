65ac01171a7bd078c87eed2aa0d3fb990977ee5c
BaronVonClop <baronvonclop@gmail.com>
2016-08-11 05:24:30 -0400

    added support for editing tickets

--- mygame/world/request.py
+++ mygame/world/request.py
@@ -75,20 +75,74 @@ def view_got_ticket(caller):
 	"""
 	|500VIEW TICKET|n
 	"""
-	text += "|/|/Here is the status of ticket #%s:" %caller.ndb._menutree.tempticketnumber
+	text += "|/|/Here is the status of your open ticket, #%s:" %caller.ndb._menutree.tempticketnumber
 	text += "|/|/Your ticket's current info is:"
 	text += "|/|/Title: %s" % caller.ndb._menutree.title
 	text += "|/|/Body: %s" % caller.ndb._menutree.body
 	options = ({"desc": "Edit Title.",
-				"goto": "edittitle"},
+				"goto": "editopentitle"},
 				{"desc": "Edit body.",
-				"goto": "editbody"},
-				{"desc": "Submit ticket.",
-				"exec": _create_ticket,
+				"goto": "editopenbody"},
+				{"desc": "Add comment.",
+				"goto": "addcomment"},
+				{"desc": "Submit edits.",
+				"exec": _edit_ticket,
 				"goto": "start"})
 				
 	return text, options
 	
+#add comment to open ticket
+def addcomment(caller):
+	text = \
+	"""
+	|500ADD COMMENT|n
+	
+	This will add a comment to your ticket.
+	
+	Use this for things like new info, or if you've been asked to by a Wizard.
+	
+	To cancel, type QUIT.
+	"""
+	options = ({"key": "_default",
+				"exec": _add_comment,
+				"goto": "view_got_ticket"})
+	return text, options
+	
+	
+#edit an open ticket's title
+def editopentitle(caller):
+	text = \
+	"""
+	Enter the title for your ticket.
+	
+	Be clear and concice on what your issue is.
+	
+	This is only a TITLE; save the details for the description!
+	"""
+	
+	options = ({"key": "_default",
+				"exec": _set_title,
+				"goto": "view_got_ticket"})
+	return text, options
+	
+	
+#edit an open ticket's body
+def editopenbody(caller):
+	text = \
+	"""
+	Enter the body for your ticket.
+	
+	Here you can give as much detail as you like! Screenshots, etc.
+	
+	Please note that if you are reporting a bug in code, you should submit an issue on github instead.
+	"""
+	
+	options = ({"key": "_default",
+				"exec": _set_body,
+				"goto": "view_got_ticket"})
+	return text, options
+	
+#edit a new ticket's title
 def edittitle(caller):
 	text = \
 	"""
@@ -104,6 +158,7 @@ def edittitle(caller):
 				"goto": "open_ticket"})
 	return text, options
 	
+#edit a new ticket's body
 def editbody(caller):
 	text = \
 	"""
@@ -119,6 +174,7 @@ def editbody(caller):
 				"goto": "open_ticket"})
 	return text, options
 	
+#pull info for ticket from view
 def _get_ticket(caller, raw_string):
 	inp = raw_string.strip()
 	target = caller.search("request", global_search=True, typeclass="typeclasses.requests.request")
@@ -126,9 +182,11 @@ def _get_ticket(caller, raw_string):
 	if not inp:
 		caller.msg("You didn't enter a number!")
 	else:
+		#check if player is ticket owner, if not then reject
 		if not caller == target.db.requestdict["reqauthor%s" % inp]:
 			caller.msg("That's not your ticket! Only wizards can see other player's tickets.")
 			return
+		#if player is ticket owner, pull the info for it
 		if caller == target.db.requestdict["reqauthor%s" % inp]:
 			caller.ndb._menutree.title = (target.db.requestdict["reqtitle%s" % inp])
 			caller.ndb._menutree.body = (target.db.requestdict["reqtext%s" % inp])
@@ -152,6 +210,14 @@ def _set_body(caller, raw_string):
 		caller.ndb._menutree.body = "%s" % inp
 		caller.msg("Title set to %s." % caller.ndb._menutree.body)
 		
+def _add_comment(caller, raw_string):
+	inp = raw_string.strip()
+	if not inp:
+		caller.msg("You didn't enter a body!")
+	else:
+		caller.ndb._menutree.body += ("|/COMMENT: %s" % inp + " - BY %s" % caller)
+		caller.msg("Comment added.")
+		
 #the Big Shit, creating the ticket
 def _create_ticket(caller):
 	#target master request item
@@ -173,3 +239,19 @@ def _create_ticket(caller):
 		caller.msg("Your ticket has been submitted as #%i" % target.db.requestnum)
 		#increase the number for the next ticket
 		target.db.requestnum += 1
+		
+		
+def _edit_ticket(caller):
+	#target master request item
+	target = caller.search("request", global_search=True, typeclass="typeclasses.requests.request")
+	caller.msg("Editing your ticket...")
+	#If the user tried to submit a ticket with no title or body, reject it
+	if caller.ndb._menutree.body == "BLANK" or caller.ndb._menutree.title == "BLANK":
+		caller.msg("You tried to submit an unfinished ticket! Try again.")
+		return
+	else:
+		#save body
+		(target.db.requestdict["reqtext%i" % caller.ndb._menutree.tempticketnumber]) = caller.ndb._menutree.body
+		#save title
+		(target.db.requestdict["reqtitle%i" % caller.ndb._menutree.tempticketnumber]) = caller.ndb._menutree.title
+		caller.msg("Your ticket, number #%s, has been edited." % caller.ndb._menutree.tempticketnumber)
