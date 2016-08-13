5490719f652e124d3fefb1283dce12294ec1b28e
BaronVonClop <baronvonclop@gmail.com>
2016-08-13 14:39:25 -0400

    finished editplayer; menu based profile building

--- mygame/world/editplayer.py
+++ mygame/world/editplayer.py
@@ -1,7 +1,9 @@
 def start(caller):
 	text = \
 	"""
-	Welcome to editplayer. This is a guided tool to set up your at-a-glance profile.
+	Welcome to editplayer. This is a guided tool to set up your at-a-glance profile and your 'look' description.
+	
+	If you're a new character, this will be looked at before approval, so be sure you are putting in your best effort!
 	
 	What would you like to do?
 	
@@ -22,6 +24,8 @@ def start(caller):
 				"goto": "edit_gender"},
 				{"desc": "Edit race.",
 				"goto": "edit_race"},
+				{"desc": "Edit age.",
+				"goto": "edit_age"},
 				{"desc": "Edit height.",
 				"goto": "edit_height"},
 				{"desc": "Edit weight.",
@@ -43,6 +47,8 @@ def edit_gender(caller):
 	Editing your gender.
 	
 	Type what you want your gender to appear as.
+	
+	Must be 20 or less characters.
 	"""
 	
 	options = ({"key": "_default",
@@ -60,3 +66,208 @@ def _edit_gender(caller, raw_string):
 			caller.db.gender = inp
 		else:
 			caller.msg("Too long. Must be under 20 characters.")
+			
+def edit_race(caller):
+	text = \
+	"""
+	Editing your race.
+	
+	Type what you want your gender to appear as.
+	
+	Please check the wiki for the available races if you are playing a non-standard pony. Outlandish things may result in failing approval!
+	"""
+	
+	options = ({"key": "_default",
+			"exec": _edit_race,
+			"goto": "start"})
+			
+	return text, options
+	
+def _edit_race(caller, raw_string):
+	inp = raw_string.strip()
+	if not inp:
+		caller.msg("You didn't enter anything!")
+	else:
+		if len(inp) <= 20:
+			caller.db.race = inp
+		else:
+			caller.msg("Too long. Must be under 20 characters.")
+			
+def edit_age(caller):
+	text = \
+	"""
+	Editing your age.
+	
+	Type what you want your age to appear as.
+	
+	Must be under 4 characters.
+	"""
+	
+	options = ({"key": "_default",
+			"exec": _edit_age,
+			"goto": "start"})
+			
+	return text, options
+	
+def _edit_age(caller, raw_string):
+	inp = raw_string.strip()
+	if not inp:
+		caller.msg("You didn't enter anything!")
+	else:
+		if len(inp) <= 4:
+			caller.db.age = inp
+		else:
+			caller.msg("Too long. Must be under 4 characters.")
+			
+def edit_height(caller):
+	text = \
+	"""
+	Editing your height.
+	
+	Type what you want your height to appear as.
+	
+	Must be 20 or less characters.
+	"""
+	
+	options = ({"key": "_default",
+			"exec": _edit_height,
+			"goto": "start"})
+			
+	return text, options
+	
+def _edit_height(caller, raw_string):
+	inp = raw_string.strip()
+	if not inp:
+		caller.msg("You didn't enter anything!")
+	else:
+		if len(inp) <= 20:
+			caller.db.height = inp
+		else:
+			caller.msg("Too long. Must be under 20 characters.")
+			
+def edit_weight(caller):
+	text = \
+	"""
+	Editing your weight.
+	
+	Type what you want your weight to appear as.
+	
+	Must be 20 or less characters.
+	"""
+	
+	options = ({"key": "_default",
+			"exec": _edit_weight,
+			"goto": "start"})
+			
+	return text, options
+	
+def _edit_weight(caller, raw_string):
+	inp = raw_string.strip()
+	if not inp:
+		caller.msg("You didn't enter anything!")
+	else:
+		if len(inp) <= 20:
+			caller.db.weight = inp
+		else:
+			caller.msg("Too long. Must be under 20 characters.")
+			
+def edit_coat(caller):
+	text = \
+	"""
+	Editing your coat.
+	
+	Type what you want your coat to appear as.
+	
+	Must be 20 or less characters.
+	"""
+	
+	options = ({"key": "_default",
+			"exec": _edit_coat,
+			"goto": "start"})
+			
+	return text, options
+	
+def _edit_coat(caller, raw_string):
+	inp = raw_string.strip()
+	if not inp:
+		caller.msg("You didn't enter anything!")
+	else:
+		if len(inp) <= 20:
+			caller.db.coat = inp
+		else:
+			caller.msg("Too long. Must be under 20 characters.")
+			
+def edit_mane(caller):
+	text = \
+	"""
+	Editing your mane.
+	
+	Type what you want your mane to appear as.
+	
+	Must be 20 or less characters.
+	"""
+	
+	options = ({"key": "_default",
+			"exec": _edit_mane,
+			"goto": "start"})
+			
+	return text, options
+	
+def _edit_mane(caller, raw_string):
+	inp = raw_string.strip()
+	if not inp:
+		caller.msg("You didn't enter anything!")
+	else:
+		if len(inp) <= 20:
+			caller.db.mane = inp
+		else:
+			caller.msg("Too long. Must be under 20 characters.")
+			
+def edit_cutie(caller):
+	text = \
+	"""
+	Editing your cutie mark.
+	
+	Type what you want your cutie mark to appear as.
+	
+	Must be 20 or less characters.
+	"""
+	
+	options = ({"key": "_default",
+			"exec": _edit_cutie,
+			"goto": "start"})
+			
+	return text, options
+	
+def _edit_cutie(caller, raw_string):
+	inp = raw_string.strip()
+	if not inp:
+		caller.msg("You didn't enter anything!")
+	else:
+		if len(inp) <= 20:
+			caller.db.cutiemark = inp
+		else:
+			caller.msg("Too long. Must be under 20 characters.")
+			
+def edit_desc(caller):
+	text = \
+	"""
+	Editing your description.
+	
+	Type a nice, detailed description! You have no limits here, so go wild.
+	
+	To include a line break for a new paragraph, use "||/".
+	"""
+	
+	options = ({"key": "_default",
+			"exec": _edit_desc,
+			"goto": "start"})
+			
+	return text, options
+	
+def _edit_desc(caller, raw_string):
+	inp = raw_string.strip()
+	if not inp:
+		caller.msg("You didn't enter anything!")
+	else:
+		caller.db.desc = inp
